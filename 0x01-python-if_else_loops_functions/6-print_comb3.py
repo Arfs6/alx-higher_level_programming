4bf3b5486525938b9b039d9a05740a598a7f7e2d
Abdulqadir ahmad <2004a3abuahmad@gmail.com>
2022-11-29 11:47:24 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for left in range(9):
-    for right in range(left, 10):
+    for right in range(left + 1, 10):
         if left == 8 and right == 9:
             continue
         print('{}{}'.format(left, right), end=", ")
