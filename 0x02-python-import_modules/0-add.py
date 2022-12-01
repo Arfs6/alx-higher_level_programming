fc133da0b8b9c9c887d8e1b1316044e3856918ea
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-12-01 13:33:32 +0100

    .

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,13 +2,7 @@
 from add_0 import add
 
 
-def test():
-    """ test if the add function is working """
+if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b))
-
-
-# execute only if file is not imported
-if __name__ == "__main__":
-    test()
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
