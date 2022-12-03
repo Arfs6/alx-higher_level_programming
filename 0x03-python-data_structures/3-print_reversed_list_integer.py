5396227057f792e3518d250564b1fe984102838a
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-12-03 14:48:26 +0100

    not using reverse method for task3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,9 +6,7 @@ def print_reversed_list_integer(my_list=[]):
     print_reversed_list_integer: prints integers in a list in reverse order
     @my_list: list to print
     """
-    my_list.reverse()
+    length = len(my_list)
 
-    for num in my_list:
-        print("{:d}".format(num))
-
-    my_list.reverse()  # setting list back to it's original state
+    for idx in range(1, length + 1):
+        print("{:d}".format(my_list[-1 * idx]))
