5ad8d4f7b8f0585d611d67a0d0faab4eaa22680e
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-12-03 13:32:56 +0100

    fixed pycodestyle error for task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def replace_in_list(my_list, idx, element):
     """
     replace_in_list: replace an element in a list
@@ -9,7 +10,7 @@ def replace_in_list(my_list, idx, element):
 
     Return: modified list
     """
-    if idx < 0 or idx > len(my_list) -1:
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
 
     my_list[idx] = element
