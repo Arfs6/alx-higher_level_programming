f3f4ba40aed915913da1c79c334520a7fdb4bd6d
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-12-03 14:52:28 +0100

    reverting task 3 to previous commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,7 +6,9 @@ def print_reversed_list_integer(my_list=[]):
     print_reversed_list_integer: prints integers in a list in reverse order
     @my_list: list to print
     """
-    length = len(my_list)
+    my_list.reverse()
 
-    for idx in range(1, length + 1):
-        print("{:d}".format(my_list[-1 * idx]))
+    for num in my_list:
+        print("{:d}".format(num))
+
+    my_list.reverse()  # setting list back to it's original state
