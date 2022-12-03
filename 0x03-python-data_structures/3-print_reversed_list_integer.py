466ae52cdb076d8a5e23a1afee9ef82ccac5a7dd
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-12-03 13:35:10 +0100

    fixed pycodestyle error fortask 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def print_reversed_list_integer(my_list=[]):
     """
     print_reversed_list_integer: prints integers in a list in reverse order
@@ -10,4 +11,4 @@ def print_reversed_list_integer(my_list=[]):
     for num in my_list:
         print("{:d}".format(num))
 
-    my_list.reverse()#setting list back to it's original state
+    my_list.reverse()  # setting list back to it's original state
