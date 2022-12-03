b55bcd0e9eda32834e2dfe007d286ec12375479d
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-12-03 13:12:26 +0100

    created task 0 to 5

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """
+    print_reversed_list_integer: prints integers in a list in reverse order
+    @my_list: list to print
+    """
+    my_list.reverse()
+
+    for num in my_list:
+        print("{:d}".format(num))
+
+    my_list.reverse()#setting list back to it's original state
