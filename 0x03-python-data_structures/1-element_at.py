224f70b7add0c18c413e4bac83af40b2453e0bf3
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-12-03 13:30:41 +0100

    fixed pycodestyle error in task 1 file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def element_at(my_list, idx):
     """
     element_at: retrieves element in a list at a given index
@@ -10,6 +11,6 @@ def element_at(my_list, idx):
     None: invalid index
     """
     if idx < 0 or idx > len(my_list) - 1:
-     return None
+        return None
 
-    return (my_list[idx])
+    return my_list[idx]
