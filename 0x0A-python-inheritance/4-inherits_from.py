8b7049ffed7af1b3db75973ea313b2bdc56459ec
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-01-09 16:47:49 +0100

    solution for task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+checks if an object is a subclass of a class
+"""
+
+
+def inherits_from(obj, a_class):
+    """check if obj is sub classof a_class"""
+    return issubclass(obj, a_class)
