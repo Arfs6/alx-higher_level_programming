15faa305e8d4095ad3fcbec88ffb7b524b45360c
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-01-11 15:57:52 +0100

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,30 +4,7 @@ create an empty class to represent a geometry
 """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """A class that represents a rectangle"""
-
-    def __init__(self, width, height):
-        """initialize the attributes of the rectangle
-        Parameter:
-        - height: height of rectangle. An integer greater than zero
-        - width: width of rectangle. An integer greater than zero
-        """
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__height = height
-        self.__width = width
-
-    def area(self):
-        """Returns the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Returns a string representation of the rectangle"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
@@ -35,9 +12,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initializes the attributes for the square"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """Returns the area of the square"""
