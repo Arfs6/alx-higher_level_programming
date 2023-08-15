befa774f2be7710c9c16f3a8cdb8c4b0909e704d
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-08-15 17:11:25 +0100

    fixed task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 
-module.exports = class extends Rectangle {
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-};
+}
+module.exports = Square;
