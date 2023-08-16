a5d297d78768d370489bd0abd002eb85a7dec50a
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-08-16 09:00:39 +0100

    finished last task

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+fs = require('fs')
+fs.readFile(process.argv[2], 'utf8', (e, source1) => {
+  if(e) {
+    console.log(e);
+    return;
+  }
+  fs.readFile(process.argv[3], 'utf8', (e, source2) => {
+    if (e) {
+      console.log(e);
+      return;
+    }
+    target = source1 + source2
+    fs.writeFile(process.argv[4], target, e => {
+      if (e) {
+        return;
+      }
+    })
+  })
+})
