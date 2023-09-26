d6e3878c5cc01fe6647eec9ef13080aef5568f78
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-09-26 14:32:23 +0100

    fixed semi standard in task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs')
-const filename = process.argv[2]
+const fs = require('fs');
+const filename = process.argv[2];
 
 fs.readFile(filename, 'utf8', (err, content) => {
   if (err) {
-    console.error(err)
+    console.error(err);
     return;
   }
-  console.log(content)
-})
+  console.log(content);
+});
