bf2b29997c36356147d21c4c2b5115314b6f7540
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-09-26 20:58:46 +0100

    fixed semistandard errors

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require("request");
+
+const url = process.argv[2];
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const results = JSON.parse(body).results;
+  let count = 0;
+  results.forEach(element => {
+    element.characters.forEach(character => {
+      if (character.endsWith("/18/")) {
+        count += 1;
+      }
+    });
+  });
+  console.log(count);
+});
